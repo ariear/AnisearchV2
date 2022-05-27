b45091d90eea690baf1fc42cee82728e7a8b3846
arie <arieinjector@gmail.com>
2022-05-27 19:18:36 +0700

    fixed responsive page detailAnime

--- src/components/Home/MainSlide.jsx
+++ src/components/Home/MainSlide.jsx
@@ -6,6 +6,7 @@ import "swiper/css/navigation";
 import { Autoplay, Pagination, Navigation } from "swiper";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 const MainSlide = () => {
     const [data,setData] = useState([])
@@ -42,12 +43,14 @@ const MainSlide = () => {
         {
             data.map((e,index) =>                
                 <SwiperSlide key={index}>
+                  <Link to={`/animedetail/${e.entry.mal_id}`} >
                     <div className="w-full rounded-xl py-32 bg-center relative" style={{ backgroundImage: `url("${e.entry.images.jpg.large_image_url}")` }}>
                         <div className="bg-[#0000007c] absolute left-0 top-0 w-full h-full text-white rounded-xl flex flex-col justify-end">
                             <p className="font-medium text-lg md:text-xl ml-5 mb-2">The best anime in history</p>
                             <p className="font-semibold text-2xl md:text-3xl ml-5 mb-6">{ e.entry.title }</p>
                         </div>
                     </div>
+                  </Link>
                 </SwiperSlide>
             )
         }
--- src/views/DetailAnime.jsx
+++ src/views/DetailAnime.jsx
@@ -33,29 +33,31 @@ const DetailAnime = () => {
     
     return (
         <div className="py-28 font-pupylinux">
-            <div className="h-[350px] bg-white bg-cover bg-center relative" style={{ backgroundImage: `url('${data.bgmain}')` }}>
-            <div className="absolute left-0 top-0 w-full h-full flex items-end bg-[#0000008a] pl-[19rem] py-5">
-                <div className="bg-[#ffffff5b] text-center py-3 px-6 rounded-lg backdrop-blur text-white mr-4">
-                    <p className="text-lg">Score</p>
-                    <p>{data.score}</p>
+            <div className="h-[350px] bg-white bg-cover bg-center relative md:mb-0 mb-56" style={{ backgroundImage: `url('${data.bgmain}')` }}>
+            <div className="absolute left-0 top-0 w-full h-full flex items-end bg-[#0000008a] md:pl-[16rem] lg:pl-[19rem] py-5">
+                <div className="md:flex hidden">
+                <div className="bg-[#ffffff5b] text-center py-3 px-6 rounded-lg backdrop-blur text-white md:mr-2 lg:mr-4">
+                    <p className="text-base lg:text-lg">Score</p>
+                    <p className="text-sm">{data.score}</p>
                 </div>
-                <div className="bg-[#ffffff5b] text-center py-3 px-6 rounded-lg backdrop-blur text-white mr-4">
-                    <p className="text-lg">Ranked</p>
-                    <p># {data.ranked}</p>
+                <div className="bg-[#ffffff5b] text-center py-3 px-6 rounded-lg backdrop-blur text-white md:mr-2 lg:mr-4">
+                    <p className="text-base lg:text-lg">Ranked</p>
+                    <p className="text-sm"># {data.ranked}</p>
                 </div>
-                <div className="bg-[#ffffff5b] text-center py-3 px-6 rounded-lg backdrop-blur text-white mr-4">
-                    <p className="text-lg">Popularity</p>
-                    <p># {data.popular}</p>
+                <div className="bg-[#ffffff5b] text-center py-3 px-6 rounded-lg backdrop-blur text-white md:mr-2 lg:mr-4">
+                    <p className="text-base lg:text-lg">Popularity</p>
+                    <p className="text-sm"># {data.popular}</p>
                 </div>
                 <div className="bg-[#ffffff5b] text-center py-3 px-6 rounded-lg backdrop-blur text-white">
-                    <p className="text-lg">Members</p>
-                    <p>{data.members}</p>
+                    <p className="text-base lg:text-lg">Members</p>
+                    <p className="text-sm">{data.members}</p>
+                </div>
                 </div>
             </div>
-            <div className="relative w-max px-16 z-20 translate-y-44">
-            <img src={data.img} className="rounded-lg mb-4" alt="" />
+            <div className="relative w-max px-4 lg:px-16 z-20 translate-y-44">
+            <img src={data.img} className="rounded-lg mb-4 w-[180px] md:w-[225px]" alt="" />
             <p className="text-white text-lg mb-2">Genres</p>
-            <div className="flex items-center flex-wrap w-[225px]">
+            <div className="flex items-center flex-wrap w-[90vw] md:w-[225px]">
                 {
                     genre.map((e,index) => 
                         <div className="bg-[#3B5089] text-white py-2 px-5 text-sm rounded-full w-max mb-2 mr-2" key={index}>{e.name}</div>
@@ -64,13 +66,15 @@ const DetailAnime = () => {
             </div>
             </div>
             </div>
-            <div className="px-16 text-white">
-                <div className="pl-60 pt-5">
+            <div className="md:px-4 lg:px-16 text-white">
+                <div className="pl-5 pr-5 md:pl-60 md:pr-0 pt-5 mb-10">
                 <p className="font-medium text-3xl mb-6">{data.title}</p>
                 <p className="text-lg mb-2">Synopsis</p>
                 <p className="text-sm mb-7">{data.synopsis}</p>
+                </div>
+                <div className="lg:pl-60 md:px-0 px-5">
                 <p className="text-2xl mb-5">Characters & Voice Actors</p>
-                <div className="grid grid-cols-4 gap-y-6">
+                <div className="grid md:grid-cols-3 xl:grid-cols-4 md:gap-x-5 xl:gap-x-0 gap-y-6">
                     {
                         char.map((e,index) =>                         
                     <div className="flex" key={index}>
